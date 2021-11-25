882369db0fdeab4dfb55c5c8afa280f7e842018c
oabustosb <oabustosb@unal.edu.co>
2021-11-25 16:56:06 +0000

    IsolationForest implementation for all datasets

---
+++ scripts/experiment_isolation.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import IsolationForest
+
+#from sklearn.experimental import enable_halving_search_cv  # noqa
+#from sklearn.model_selection import HalvingGridSearchCV
+
+from calculate_metrics import calculate_metrics
+
+def experiment_isolation(X_train, y_train, X_test, y_test, settings, mlflow):
+    
+    for i, setting in enumerate(settings):
+
+        print(f"experiment_dmkdc {i} setting {setting}")
+        with mlflow.start_run(run_name=setting["z_run_name"]):
+
+            model = IsolationForest(n_estimators=setting["z_n_estimators"], max_samples=setting["z_max_samples"], 
+                                    contamination=setting["z_nu"], random_state=setting["z_random_state"])
+            model.fit(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            y_test = y_test.flatten()
+            y_test_pred[ y_test_pred == 1 ] = setting["z_labels"][0]
+            y_test_pred[ y_test_pred == -1 ] = setting["z_labels"][1]            
+
+            metrics = calculate_metrics(y_test, y_test_pred)
+
+            mlflow.log_params(setting)
+            mlflow.log_metrics(metrics)
+            print(f"experiment_dmkdc {i} metrics {metrics}")
--- scripts/make_experiment.py
+++ scripts/make_experiment.py
@@ -1,8 +1,9 @@
 from experiment_oneclass import experiment_oneclass
-
-
+from experiment_isolation import experiment_isolation
 
 def make_experiment(algorithm, X_train, y_train, X_test, y_test, settings, mlflow):
     
     if algorithm == "oneclass":
         experiment_oneclass(X_train, y_train, X_test, y_test, settings, mlflow)
+    if algorithm == "isolation":
+        experiment_isolation(X_train, y_train, X_test, y_test, settings, mlflow)
