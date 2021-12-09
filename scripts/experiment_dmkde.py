ca7d6bad8e64988620a5113c0e7f8800bceba51e
oabustosb <oabustosb@unal.edu.co>
2021-12-09 00:15:34 +0000

    Fixing bugs into both DMKDE experiments

--- scripts/experiment_dmkde.py
+++ scripts/experiment_dmkde.py
@@ -16,7 +16,13 @@ def experiment_dmkde(X_train, y_train, X_test, y_test, settings, mlflow):
                                          gamma=setting["z_gamma"], random_state=setting["z_random_state"])
             qmd = models.QMDensity(fm_x, setting["z_rff_components"])
             qmd.compile()
-            qmd.fit(X_train, epochs=1, batch_size=setting["z_batch_size"], verbose=0)
+
+            X = []
+            for i in range(len(y_train)):
+                if y_train[i] == 0: X.append(X_train[i])
+            print(len(X))
+            
+            qmd.fit(np.array(X), epochs=1, batch_size=setting["z_batch_size"], verbose=0)
             
             y_test_pred = qmd.predict(X_test)
 
--- scripts/experiment_dmkde_sgd.py
+++ scripts/experiment_dmkde_sgd.py
@@ -16,13 +16,18 @@ def experiment_dmkde_sgd(X_train, y_train, X_test, y_test, settings, mlflow):
 
             optimizer = tf.keras.optimizers.Adam(learning_rate=setting["z_learning_rate"])
 
-            rho, num_eig = calculate_eigs(X_train, setting, i)
+            X = []
+            for i in range(len(y_train)):
+                if y_train[i] == 0: X.append(X_train[i])
+            print(len(X))
+
+            rho, num_eig = calculate_eigs(np.array(X), setting, i)
 
             qmkde = models.QMDensitySGD(X_train.shape[1], setting["z_rff_components"], num_eig=num_eig, 
                                         gamma=setting["z_gamma"], random_state=setting["z_random_state"])
             qmkde.compile(optimizer)
             eig_vals = qmkde.set_rho(rho)
-            qmkde.fit(X_train, epochs=setting["z_epochs"], batch_size=setting["z_batch_size"], verbose=0)
+            qmkde.fit(np.array(X), epochs=setting["z_epochs"], batch_size=setting["z_batch_size"], verbose=0)
             
 
             y_test_pred = qmkde.predict(X_test)
